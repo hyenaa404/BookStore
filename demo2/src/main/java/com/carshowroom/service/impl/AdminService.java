d9bd7287ce6263a2721098981ef011e7227d02c6
Nguyen Hue Suong <nhs211306@gmail.com>
2024-05-23 02:00:44 +0700

    remove

--- demo2/src/main/java/com/carshowroom/service/impl/AdminService.java
+++
@@ -1,6 +0,0 @@
-package com.carshowroom.service.impl;
-
-import com.carshowroom.service.IAdminService;
-
-public class AdminService implements IAdminService {
-}
--- demo2/src/main/java/com/carshowroom/service/impl/ContactService.java
+++
@@ -1,6 +0,0 @@
-package com.carshowroom.service.impl;
-
-import com.carshowroom.service.IContactService;
-
-public class ContactService implements IContactService {
-}
